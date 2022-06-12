1cdbb7aaac275efe6047c896bcefd725822d1039
mandos1995 <mandos19950620@gmail.com>
2022-06-13 00:51:17 +0900

    💄 심플웨더 메인화면 UI 추가

--- Project/2. 심플웨더/SimpleWeather/SimpleWeather/WeatherViewController.swift
+++ Project/2. 심플웨더/SimpleWeather/SimpleWeather/WeatherViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WeatherViewController.swift
 //  SimpleWeather
 //
 //  Created by 김민석 on 2022/06/13.
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WeatherViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+    
     }
-
-
+    
 }
 
