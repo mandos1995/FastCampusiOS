6da2462fbb7938666c216a430fa0eaffbe2dd063
mandos1995 <mandos19950620@gmail.com>
2022-06-13 01:05:07 +0900

    ✨ 버튼 클릭시 랜덤으로 이미지, 텍스트 변경하는 기능 구현 및 심플웨더 프로젝트 완성

--- Project/2. 심플웨더/SimpleWeather/SimpleWeather/WeatherViewController.swift
+++ Project/2. 심플웨더/SimpleWeather/SimpleWeather/WeatherViewController.swift
@@ -8,11 +8,26 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
-
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var weatherImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    let cities = ["Seoul", "Tokyo", "LA", "Seattle"]
+    let weathers = ["cloud.fill", "sun.max.fill", "wind", "cloud.sun.rain.fill"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     
+    }
+    
+    @IBAction func changeButtonTapped(_ sender: UIButton) {
+        cityLabel.text = cities.randomElement()!
+        let imageName = weathers.randomElement()!
+        weatherImageView.image = UIImage(systemName: imageName)?.withRenderingMode(.alwaysOriginal)
+        
+        let randomTemp = Int.random(in: 10..<30)
+        temperatureLabel.text = "\(randomTemp)°"
     }
     
 }
