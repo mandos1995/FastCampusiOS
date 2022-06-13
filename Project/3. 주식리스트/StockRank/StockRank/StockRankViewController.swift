a3cd7a7c522f029164bb133f7342d498d8d70ae0
mandos1995 <mandos19950620@gmail.com>
2022-06-13 14:22:11 +0900

    💄 주식리스트 메인화면 UI 추가

---
+++ Project/3. 주식리스트/StockRank/StockRank/StockRankViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StockRankViewController.swift
+//  StockRank
+//
+//  Created by 김민석 on 2022/06/13.
+//
+
+import UIKit
+
+class StockRankViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
