157afe691d69b6d24c32da66ab7117348cc61fdf
mandos1995 <mandos19950620@gmail.com>
2022-06-13 14:47:39 +0900

    ✨ CollectionViewCell 연동

---
+++ Project/3. 주식리스트/StockRank/StockRank/StockRankCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  StockRankCollectionViewCell.swift
+//  StockRank
+//
+//  Created by 김민석 on 2022/06/13.
+//
+
+import UIKit
+
+class StockRankCollectionViewCell: UICollectionViewCell {
+    
+}
--- Project/3. 주식리스트/StockRank/StockRank/StockRankViewController.swift
+++ Project/3. 주식리스트/StockRank/StockRank/StockRankViewController.swift
@@ -8,10 +8,38 @@
 import UIKit
 
 class StockRankViewController: UIViewController {
-
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    let stockList: [StockModel] = StockModel.list
+    
+    // Data, Presentation, Layout
+    // Data - 어떤 데이터?
+    // Presentation - 셀을 어떻게 표현?
+    // Layout - 셀을 어떻게 배치?
+    
+    // protocol -
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.dataSource = self
+        collectionView.delegate = self
 
     }
+}
+
+extension StockRankViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return stockList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "StockRankCollectionViewCell", for: indexPath)
+        return cell
+    }
+    
+}
+
+extension StockRankViewController: UICollectionViewDelegateFlowLayout {
+    
 }
