703656d82a2234d94a20e3d53ec866ed45ea6577
mandos1995 <mandos19950620@gmail.com>
2022-06-13 20:24:51 +0900

    💄 채팅화면 UI 추가 및 cell UI 업데이트 기능 구현

---
+++ Project/4. 채팅리스트/ChatList/ChatList/ChatListCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ChatListCollectionViewCell.swift
+//  ChatList
+//
+//  Created by 김민석 on 2022/06/13.
+//
+
+import UIKit
+
+class ChatListCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbnail: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var chatLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    func configure(_ chat: Chat) {
+        thumbnail.image = UIImage(named: chat.name)
+        nameLabel.text = chat.name
+        chatLabel.text = chat.chat
+        dateLabel.text = chat.date
+    }
+}
---
+++ Project/4. 채팅리스트/ChatList/ChatList/ChatListViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ChatListViewController.swift
+//  ChatList
+//
+//  Created by 김민석 on 2022/06/13.
+//
+
+import UIKit
+
+class ChatListViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let chatList: [Chat] = Chat.list
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+}
+
+extension ChatListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return chatList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ChatListCollectionViewCell", for: indexPath) as? ChatListCollectionViewCell else { return UICollectionViewCell () }
+        cell.configure(chatList[indexPath.item])
+        return cell
+    }
+    
+    
+}
+
+extension ChatListViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.bounds.width, height: 100)
+    }
+}
