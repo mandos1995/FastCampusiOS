a4dff337442599e1d8fdf9a7c9227292c81d3fe1
mandos1995 <mandos19950620@gmail.com>
2022-06-14 18:09:15 +0900

    💄 메인화면 UI 구현

---
+++ Project/6. 인스타 검색뷰/InstaSearchView/InstaSearchView/ResultCell.swift
@@ -0,0 +1,21 @@
+//
+//  ResultCell.swift
+//  InstaSearchView
+//
+//  Created by 김민석 on 2022/06/14.
+//
+
+import UIKit
+
+class ResultCell: UICollectionViewCell {
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        thumbnailImageView.image = nil
+    }
+    
+    func configure(_ imageName: String) {
+        thumbnailImageView.image = UIImage(named: imageName)
+    }
+}
---
+++ Project/6. 인스타 검색뷰/InstaSearchView/InstaSearchView/SearchViewController.swift
@@ -0,0 +1,54 @@
+//
+//  SearchViewController.swift
+//  InstaSearchView
+//
+//  Created by 김민석 on 2022/06/14.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        if let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            flowLayout.estimatedItemSize = .zero
+        }
+    }
+}
+
+extension SearchViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 24
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ResultCell", for: indexPath) as? ResultCell else {
+            return UICollectionViewCell()
+        }
+        let imageName = "animal\(indexPath.item + 1)"
+        cell.configure(imageName)
+        return cell
+    }
+}
+
+extension SearchViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let interItemSpacing: CGFloat = 1
+        let width = (collectionView.bounds.width - interItemSpacing * 2) / 3
+        let height = width
+        return CGSize(width: width, height: height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+}
