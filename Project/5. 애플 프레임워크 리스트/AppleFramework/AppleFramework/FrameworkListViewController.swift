7b55db2347f23554fb6f3c5b351b503945c9dc57
mandos1995 <mandos19950620@gmail.com>
2022-06-14 11:12:27 +0900

    💄 Inset, padding 등 UI 수정

--- Project/5. 애플 프레임워크 리스트/AppleFramework/AppleFramework/FrameworkListViewController.swift
+++ Project/5. 애플 프레임워크 리스트/AppleFramework/AppleFramework/FrameworkListViewController.swift
@@ -16,6 +16,12 @@ class FrameworkListViewController: UIViewController {
         super.viewDidLoad()
         collectionView.delegate = self
         collectionView.dataSource = self
+        
+        if let flowlayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            flowlayout.estimatedItemSize = .zero
+        }
+        
+        collectionView.contentInset = UIEdgeInsets(top: 20, left: 16, bottom: 0, right: 16)
     }
 
 }
@@ -40,7 +46,8 @@ extension FrameworkListViewController: UICollectionViewDataSource {
 extension FrameworkListViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let interItemSpacing: CGFloat = 10
-        let width = (collectionView.bounds.width - interItemSpacing * 2) / 3
+        let padding: CGFloat = 16
+        let width = (collectionView.bounds.width - interItemSpacing * 2 - padding * 2) / 3
         let height = width * 1.5
         return CGSize(width: width, height: height)
     }
