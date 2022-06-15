346436beb54b1c30652f848abeee4e9b4c8ee009
mandos1995 <mandos19950620@gmail.com>
2022-06-15 14:36:24 +0900

    🎉 NRC 온보딩 프로젝트 시작

---
+++ Project/7. NRC 온보딩/NRCOnboarding/NRCOnboarding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NRCOnboarding
+//
+//  Created by 김민석 on 2022/06/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
