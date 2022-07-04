05d8d08e0e8e3d0eaeaadd12ec0c44869c137bb2
mandos1995 <mandos19950620@gmail.com>
2022-07-04 11:32:45 +0900

    💄 메인화면 UI 구현

---
+++ Project/9. 집중을 위한 명상 컨텐츠 리스트/HeadSpaceForcus/HeadSpaceForcus/FocusCell.swift
@@ -0,0 +1,12 @@
+//
+//  FocusCell.swift
+//  HeadSpaceForcus
+//
+//  Created by 김민석 on 2022/07/04.
+//
+
+import UIKit
+
+class FocusCell: UICollectionViewCell {
+    
+}
--- Project/9. 집중을 위한 명상 컨텐츠 리스트/HeadSpaceForcus/HeadSpaceForcus/FocusViewController.swift
+++ Project/9. 집중을 위한 명상 컨텐츠 리스트/HeadSpaceForcus/HeadSpaceForcus/FocusViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FocusViewController.swift
 //  HeadSpaceForcus
 //
 //  Created by 김민석 on 2022/07/04.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FocusViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+    
     }
 
-
 }
-
