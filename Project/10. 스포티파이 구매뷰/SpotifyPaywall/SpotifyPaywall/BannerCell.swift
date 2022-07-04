29db2d3530297453f2ea536b05065caf7efe46a1
mandos1995 <mandos19950620@gmail.com>
2022-07-04 16:31:37 +0900

    💄 메인 화면 UI 구현

--- Project/10. 스포티파이 구매뷰/SpotifyPaywall/SpotifyPaywall/BannerCell.swift
+++ Project/10. 스포티파이 구매뷰/SpotifyPaywall/SpotifyPaywall/BannerCell.swift
@@ -9,4 +9,13 @@ import UIKit
 
 class BannerCell: UICollectionViewCell {
     
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    func configure(_ info: BannerInfo) {
+        titleLabel.text = info.title
+        descriptionLabel.text = info.description
+        thumbnailImageView.image = UIImage(named: info.imageName)
+    }
 }
--- Project/10. 스포티파이 구매뷰/SpotifyPaywall/SpotifyPaywall/PaywallViewController.swift
+++ Project/10. 스포티파이 구매뷰/SpotifyPaywall/SpotifyPaywall/PaywallViewController.swift
@@ -12,8 +12,15 @@ import UIKit
 // 과제: 아래 애플 샘플 코드 다운받아서 돌려보기  https://developer.apple.com/documentation/uikit/views_and_controls/collection_views/implementing_modern_collection_views
 
 class PaywallViewController: UIViewController {
-
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let bannerInfo: [BannerInfo] = BannerInfo.list
+    let colors: [UIColor] = [.systemPurple, .systemOrange, .systemPink, .systemRed]
+    
      override func viewDidLoad() {
         super.viewDidLoad()
+         
+         
     }
 }
