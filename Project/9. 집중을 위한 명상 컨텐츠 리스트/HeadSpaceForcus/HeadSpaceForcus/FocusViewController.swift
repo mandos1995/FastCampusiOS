00181170c01af3ba390cd63ec3234a63c830d7c2
mandos1995 <mandos19950620@gmail.com>
2022-07-04 11:50:23 +0900

    ✨ DataSource, SnapShot, Layout 기능 구현

--- Project/9. 집중을 위한 명상 컨텐츠 리스트/HeadSpaceForcus/HeadSpaceForcus/FocusViewController.swift
+++ Project/9. 집중을 위한 명상 컨텐츠 리스트/HeadSpaceForcus/HeadSpaceForcus/FocusViewController.swift
@@ -8,11 +8,45 @@
 import UIKit
 
 class FocusViewController: UIViewController {
-
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var items: [Focus] = Focus.list
+    typealias Item = Focus
+    enum Section {
+        case main
+    }
+    
+    var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView, cellProvider: { collectionView, indexPath, item in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FocusCell", for: indexPath) as? FocusCell else {
+                return nil
+            }
+//            cell.configure(item)
+            return cell
+        })
+        
+        var snapShot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapShot.appendSections([.main])
+        snapShot.appendItems(items, toSection: .main)
+        dataSource.apply(snapShot)
+        
+        collectionView.collectionViewLayout = layout()
+    }
     
+    private func layout() -> UICollectionViewCompositionalLayout {
+        
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(50))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(50))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
     }
 
 }
