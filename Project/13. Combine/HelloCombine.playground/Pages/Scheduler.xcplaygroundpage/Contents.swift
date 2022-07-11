9b5f3068cd70be43425f796e24c1ef040eae9436
mandos1995 <mandos19950620@gmail.com>
2022-07-11 14:04:35 +0900

    🚧 Scheduler 실습

---
+++ Project/13. Combine/HelloCombine.playground/Pages/Scheduler.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+import Foundation
+import Combine
+
+// MARK: Scheduler
+
+let arrPublisher = [1, 2, 3].publisher
+
+let queue = DispatchQueue(label: "custom")
+
+let subscription = arrPublisher
+    .subscribe(on: queue)
+    .map { value -> Int in
+        print("transform: \(value), thread: \(Thread.current)")
+        return value
+    }
+    .receive(on: DispatchQueue.main)
+    .sink { value in
+    print("Receive Value: \(value), theard: \(Thread.current)")
+}
