fa627dacd08a54f93c6cabaa1306f3103918846b
mandos1995 <mandos19950620@gmail.com>
2022-08-22 14:46:04 +0900

    ⚡ [심플웨더] 연속으로 같은 요소가 나오면 다시뽑는 기능 구현 (아쉬운점 수정)

--- Project/2. 심플웨더/SimpleWeather/SimpleWeather/WeatherViewController.swift
+++ Project/2. 심플웨더/SimpleWeather/SimpleWeather/WeatherViewController.swift
@@ -11,23 +11,34 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var weatherImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
+    var prevCityText = ""
+    var prevWeatherImageName = ""
     
     let cities = ["Seoul", "Tokyo", "LA", "Seattle"]
     let weathers = ["cloud.fill", "sun.max.fill", "wind", "cloud.sun.rain.fill"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-    
     }
     
     @IBAction func changeButtonTapped(_ sender: UIButton) {
-        cityLabel.text = cities.randomElement()!
-        let imageName = weathers.randomElement()!
-        weatherImageView.image = UIImage(systemName: imageName)?.withRenderingMode(.alwaysOriginal)
+        // MARK: 동일한 요소가 연속으로 나오는 경우 다시 뽑음
+        var cityText = cities.randomElement()!
+        var imageName = weathers.randomElement()!
+        while prevCityText == cityText {
+            cityText = cities.randomElement()!
+        }
+        while imageName == prevWeatherImageName {
+            imageName = weathers.randomElement()!
+        }
         
+        cityLabel.text = cityText
+        weatherImageView.image = UIImage(systemName: imageName)?.withRenderingMode(.alwaysOriginal)
         let randomTemp = Int.random(in: 10..<30)
         temperatureLabel.text = "\(randomTemp)°"
+        
+        prevCityText = cityText
+        prevWeatherImageName = imageName
     }
     
 }
