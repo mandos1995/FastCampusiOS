02f9c224a69705eb3f0f15a64b98e61e44b4b0e1
mandos1995 <mandos19950620@gmail.com>
2022-08-29 17:58:52 +0900

    ✨ 메인화면 UI 구현 및 Searchbar 생성

--- Project/16.깃헙 프로필/GithubUserProfile/GithubUserProfile/SearchViewController.swift
+++ Project/16.깃헙 프로필/GithubUserProfile/GithubUserProfile/SearchViewController.swift
@@ -11,6 +11,9 @@ import Kingfisher
 
 class UserProfileViewController: UIViewController {
     
+    @Published private(set) var user: UserProfile?
+    var subscriptions = Set<AnyCancellable>()
+    
     @IBOutlet weak var thumbnail: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var loginLabel: UILabel!
@@ -19,5 +22,48 @@ class UserProfileViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+        embedSearchControl()
+        bind()
+    }
+    
+    private func setupUI() {
+        thumbnail.layer.cornerRadius = thumbnail.layer.borderWidth / 2
+        
+    }
+    
+    private func embedSearchControl() {
+        self.navigationItem.title = "Search"
+        
+        let searchControl = UISearchController(searchResultsController: nil)
+        searchControl.hidesNavigationBarDuringPresentation = false
+        searchControl.searchBar.placeholder = "mandos1995"
+        searchControl.searchResultsUpdater = self
+        searchControl.searchBar.delegate = self
+        self.navigationItem.searchController = searchControl
+    }
+    
+    private func bind() {
+        $user
+            .receive(on: RunLoop.main)
+            .sink { [weak self] result in
+                self?.update(result)
+            }.store(in: &subscriptions)
+    }
+    
+    private func update(_ user: UserProfile?) {
+        
+    }
+}
+extension UserProfileViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        let keyword = searchController.searchBar.text
+        print("search: \(keyword)")
+    }
+}
+
+extension UserProfileViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("button clicked: \(searchBar.text)")
     }
 }
