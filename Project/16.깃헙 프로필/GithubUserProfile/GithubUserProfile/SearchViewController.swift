9e1c6e911a3116e76b6e1860dba1dc486af483a5
mandos1995 <mandos19950620@gmail.com>
2022-10-05 12:20:03 +0900

    ✨ URL Session 사용해 UserProfile 받아오는 기능 구현

--- Project/16.깃헙 프로필/GithubUserProfile/GithubUserProfile/SearchViewController.swift
+++ Project/16.깃헙 프로필/GithubUserProfile/GithubUserProfile/SearchViewController.swift
@@ -52,7 +52,20 @@ class UserProfileViewController: UIViewController {
     }
     
     private func update(_ user: UserProfile?) {
-        
+        guard let user = user else {
+            self.nameLabel.text = "n/a"
+            self.loginLabel.text = "n/a"
+            self.followerLabel.text = "n/a"
+            self.followingLabel.text = "n/a"
+            self.thumbnail.image = nil
+            return
+        }
+        self.nameLabel.text = user.name
+        self.loginLabel.text = user.login
+        self.followerLabel.text = "follower: \(user.followers)"
+        self.followingLabel.text = "following: \(user.following)"
+        self.thumbnail.image = nil
+
     }
 }
 extension UserProfileViewController: UISearchResultsUpdating {
@@ -65,5 +78,43 @@ extension UserProfileViewController: UISearchResultsUpdating {
 extension UserProfileViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         print("button clicked: \(searchBar.text)")
+        
+        guard let keyword = searchBar.text, !keyword.isEmpty else { return }
+        "https://api.github.com/users/\(keyword)"
+        let base = "https://api.github.com/"
+        let path = "users/\(keyword)"
+        let params: [String: String] = [:]
+        let header: [String: String] = ["Content-Type": "application/json"]
+        
+        var urlComponents = URLComponents(string: base + path)!
+        let queryItems = params.map { (key: String, value: String) in
+            return URLQueryItem(name: key, value: value)
+        }
+        urlComponents.queryItems = queryItems
+        
+        var request = URLRequest(url: urlComponents.url!)
+        header.forEach { (key: String, value: String) in
+            request.addValue(value, forHTTPHeaderField: key)
+        }
+        
+        URLSession.shared
+            .dataTaskPublisher(for: request)
+            .tryMap { result -> Data in
+                guard let response = result.response as? HTTPURLResponse,
+                      (200..<300).contains(response.statusCode) else {
+                    let response = result.response as? HTTPURLResponse
+                    let statusCode = response?.statusCode ?? -1
+                    throw NetworkError.responseError(statusCode: statusCode)
+                }
+                return result.data
+            }
+            .decode(type: UserProfile.self, decoder: JSONDecoder())
+            .receive(on: RunLoop.main)
+            .sink { completion in
+                print(completion)
+            } receiveValue: { user in
+                self.user = user
+            }.store(in: &subscriptions)
+
     }
 }
