5e64f91bbd3423b7c626e71ce0b03872c9fa14f7
mandos1995 <mandos19950620@gmail.com>
2022-10-06 16:59:04 +0900

    🎉 깃헙 사용자 검색 프로젝트 시작

---
+++ Project/17.깃헙 사용자 검색/GithubUserSearch/GithubUserSearch/View/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  GithubUserSearch
+//
+//  Created by 김민석 on 2022/10/06.
+//
+
+import UIKit
+import Combine
+
+class SearchViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
