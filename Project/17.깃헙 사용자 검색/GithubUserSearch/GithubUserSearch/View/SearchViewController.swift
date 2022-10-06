92eb328400e9ed760404bb8aeb7588b2450eb30c
mandos1995 <mandos19950620@gmail.com>
2022-10-06 17:57:31 +0900

    ✨ collectionView layout, datasource, snapshot 설정

--- Project/17.깃헙 사용자 검색/GithubUserSearch/GithubUserSearch/View/SearchViewController.swift
+++ Project/17.깃헙 사용자 검색/GithubUserSearch/GithubUserSearch/View/SearchViewController.swift
@@ -10,9 +10,76 @@ import Combine
 
 class SearchViewController: UIViewController {
     
+    @Published private(set) var users: [SearchResult] = []
+    var subscriptions = Set<AnyCancellable>()
+    
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    enum Section {
+        case main
+    }
+    typealias Item = SearchResult
+    
+    var datasource: UICollectionViewDiffableDataSource<Section, Item>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        embedSearchControl()
+        configureCollectionView()
+    }
+    
+    private func embedSearchControl() {
+        self.navigationItem.title = "Search"
+        
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.placeholder = "mandos1995"
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        self.navigationItem.searchController = searchController
+    }
+    
+    private func configureCollectionView() {
+        datasource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView,
+                                                                       cellProvider: { collectionView, indexPath, item in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ResultCell", for: indexPath) as? ResultCell else { return nil }
+            cell.user.text = item.login
+            return cell
+        })
+        
+        collectionView.collectionViewLayout = layout()
+    }
+    
+    private func layout() -> UICollectionViewCompositionalLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(60))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(60))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        return UICollectionViewCompositionalLayout(section: section)
+    }
+    
+    private func bind() {
+        $users
+            .receive(on: RunLoop.main)
+            .sink { users in
+                var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+                snapshot.appendSections([.main])
+                snapshot.appendItems(users, toSection: .main)
+                self.datasource.apply(snapshot)
+            }.store(in: &subscriptions)
+    }
+}
+
+extension SearchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        let keyword = searchController.searchBar.text
+        print("search: ", keyword)
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("button clicked: ", searchBar.text)
     }
 }
