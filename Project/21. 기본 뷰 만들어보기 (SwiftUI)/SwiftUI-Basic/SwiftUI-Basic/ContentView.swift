59b6a5fd1cfabc17a831db0e8d90cf42e656e27f
mandos1995 <mandos19950620@gmail.com>
2023-01-29 23:15:16 +0900

    ✨ Stack을 사용해 레이아웃을 적용

--- Project/21. 기본 뷰 만들어보기 (SwiftUI)/SwiftUI-Basic/SwiftUI-Basic/ContentView.swift
+++ Project/21. 기본 뷰 만들어보기 (SwiftUI)/SwiftUI-Basic/SwiftUI-Basic/ContentView.swift
@@ -9,9 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-//        ImageView()
-//        ButtonView()
-        TextView()
+        VStack {
+            ImageView()
+            ButtonView()
+            TextView()
+            
+            Spacer()
+            
+            HStack {
+                ImageView()
+                ButtonView()
+                TextView()
+            }
+            ZStack {
+                ImageView()
+                ButtonView()
+                TextView()
+            }
+        }
     }
 }
 
