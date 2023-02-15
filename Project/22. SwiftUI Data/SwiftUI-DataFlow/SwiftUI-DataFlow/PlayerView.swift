bccb3e4c71b0226b3aab47ddec603e68af37ee28
mandos1995 <mandos19950620@gmail.com>
2023-02-15 18:41:46 +0900

    ✨ 파일 분류 및 프로젝트 완성

--- Project/22. SwiftUI Data/SwiftUI-DataFlow/SwiftUI-DataFlow/PlayerView.swift
+++ Project/22. SwiftUI Data/SwiftUI-DataFlow/SwiftUI-DataFlow/PlayerView.swift
@@ -24,36 +24,6 @@ struct PlayerView: View {
     }
 }
 
-struct PlayButton: View {
-    
-    @Binding var isPlaying: Bool
-    
-    var body: some View {
-        Button {
-            self.isPlaying.toggle()
-        } label: {
-            Image(systemName: isPlaying ? "pause.circle" : "play.circle")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(height: 80)
-                .foregroundColor(.primary)
-        }
-    }
-}
-
-struct PlayingStatusView: View {
-    
-    @Binding var isPlaying: Bool
-    
-    var body: some View {
-        Image(systemName: isPlaying ? "sun.max.fill" : "sun.max")
-            .resizable()
-            .renderingMode(.original)
-            .aspectRatio(contentMode: .fit)
-            .frame(width: 80, height: 80)
-    }
-}
-
 struct PlayerView_Previews: PreviewProvider {
     static var previews: some View {
         PlayerView(episode: Episode.list.first!)
