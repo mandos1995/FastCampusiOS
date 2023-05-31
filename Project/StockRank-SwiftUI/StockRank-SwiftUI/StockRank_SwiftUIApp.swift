27001fcbda0c0dba3ad4b55470c5d91f4c435fda
mandos1995 <mandos19950620@gmail.com>
2023-05-31 16:08:08 +0900

    🎉 [24. 주식리스트 (SwiftUI)] 프로젝트 생성

---
+++ Project/StockRank-SwiftUI/StockRank-SwiftUI/StockRank_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockRank_SwiftUIApp.swift
+//  StockRank-SwiftUI
+//
+//  Created by 김민석 on 2023/05/31.
+//
+
+import SwiftUI
+
+@main
+struct StockRank_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
