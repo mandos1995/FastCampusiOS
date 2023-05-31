5f68b8534a5555da2698ffac84c24043864ff614
mandos1995 <mandos19950620@gmail.com>
2023-05-31 14:17:56 +0900

    🎉 [23. 심볼롤러 (SwiftUI)] 프로젝트 생성

---
+++ Project/23. 심볼롤러 (SwiftUI)/SymbolRoller-SwiftUI/SymbolRoller-SwiftUI/SymbolRoller_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymbolRoller_SwiftUIApp.swift
+//  SymbolRoller-SwiftUI
+//
+//  Created by 김민석 on 2023/05/31.
+//
+
+import SwiftUI
+
+@main
+struct SymbolRoller_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
