3a7ec0a2ff20bf0d819d55074a0932c0ed57b6ac
mandos1995 <mandos19950620@gmail.com>
2023-05-31 16:27:05 +0900

    💄 주식리스트 Cell UI 구현

--- Project/24. 주식리스트 (SwiftUI)/StockRank-SwiftUI/StockRank-SwiftUI/StockRankRow.swift
+++ Project/24. 주식리스트 (SwiftUI)/StockRank-SwiftUI/StockRank-SwiftUI/StockRankRow.swift
@@ -10,9 +10,42 @@ import SwiftUI
 struct StockRankRow: View {
     var body: some View {
         HStack {
-            Text("TSLA")
+            Text("1")
+                .font(.system(size: 16, weight: .bold))
+                .frame(width: 30)
+                .foregroundColor(.blue)
+            
             Image("TSLA")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            
+            VStack(alignment: .leading, spacing: 5) {
+                Text("TSLA")
+                    .font(.system(size: 12))
+                    
+                HStack {
+                    Text("1,000,000 원")
+                        .font(.system(size: 12))
+                    Text("+0.4%")
+                        .foregroundColor(.red)
+                }
+            }
+            .foregroundColor(.white)
+            
+            Spacer()
+            
+            Image(systemName: "heart.fill")
+                .resizable()
+                .renderingMode(.template)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+                .foregroundColor(.gray)
         }
+        .padding()
+        .frame(maxWidth: .infinity)
+        .background(.black)
+        
     }
 }
 
