be29b46c6dee7b1f10af5e0ff8669566b9c42022
mandos1995 <mandos19950620@gmail.com>
2023-06-15 21:24:58 +0900

    🎉 [27. EnvironmentObject] 프로젝트 생성

---
+++ Project/27. EnvironmentObject/EnvironmentObjectTest/EnvironmentObjectTest/EnvironmentObjectTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentObjectTestApp.swift
+//  EnvironmentObjectTest
+//
+//  Created by 김민석 on 2023/06/15.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentObjectTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
